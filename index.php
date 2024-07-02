1a8ab8fe1835f646c05bc2833d2ee4c0f6e03fbd
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-02 15:04:47 +0200

    Importato file della classe di cui ho bisogno. Creta variabile del film. Messo come argomenti parametri scelti. Creata seconda variabile. Creato il doctype. Messa la funzione display alle rispettive variabili.

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php 
 
+require_once __DIR__ . '/class/Movie.php';
+
+$scream = new Movie ("Scream", "Horror", 1996, "Wes Craven", "7.4/10");
+
+$myHeroAcademia = new Movie ("My Hero Academia The Movie Two Heroes", "Animazione", 2018, "Kenji Nagasaki", "7.3/10");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie</title>
+</head>
+<body>
+    <main>
+        <ul>
+            <li>
+                <?php $scream->display() ?>
+            </li>
+            <li>
+                <?php $myHeroAcademia->display() ?>
+            </li>
+        </ul>
+    </main>
+</body>
+</html>
